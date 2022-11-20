7ac1b52a97b465bb21f69dabea76b42410f03ef0
25092003Hung <hungprodns1234@gmail.com>
2022-11-20 23:01:41 +0700

    chinh database

--- DemoDB2/DemoDB2/Models/EntitiesDB.Designer.cs
+++ DemoDB2/DemoDB2/Models/EntitiesDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\PC\Desktop\New folder (3)\DemoDB2\DemoDB2\Models\EntitiesDB.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\PC\Desktop\DBstore\DemoDB2\DemoDB2\Models\EntitiesDB.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
